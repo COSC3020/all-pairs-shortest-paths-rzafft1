103ef3a1855927ed4144097fe9f2e831f16ab195
Ryan Zafft <123418615+rzafft1@users.noreply.github.com>
2023-11-02 13:59:37 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,31 @@
-function allPairsShortestPaths(graph) {
-    return [];
+function allPairsShortestPaths(graph) 
+{
+    var dist = Array(graph.length).fill().map(() => Array(graph.length).fill(Infinity))
+    
+    for (let u = 0; u < dist.length; u++)
+    {
+        dist[u][u] = 0
+        for (let v = 0; v < dist.length; v++)
+        {
+            if (graph[u][v] != Infinity)
+            {
+                dist[u][v] = graph[u][v]
+            }
+        }
+    }
+
+    for (let k = 0; k < dist.length; k++)
+    {
+        for (let i = 0; i < dist.length; i++)
+        {
+            for (let j = 0; j < dist.length; j++)
+            {
+                if (dist[i][j] > dist[i][k] + dist[k][j])
+                {
+                    dist[i][j] = dist[i][k] + dist[k][j]
+                }
+            }
+        }
+    }
+    return dist;
 }
